c841feb0dd5eeb490c78a36ce2f699e55d7b6386
iOSSwetaTest <119624565+iOSSwetaTest@users.noreply.github.com>
2023-03-29 14:56:36 -0600

    First Commit

---
+++ StarWars/StarWars/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StarWars
+//
+//  Created by shweta dodiya on 2023-03-29.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
